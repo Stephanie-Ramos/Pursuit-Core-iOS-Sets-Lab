e6a26256233d58d082b2f854f1a83b29e18e5be4
Stephanie Ramos <stephanien.ramos@Stephanies-MacBook-Pro-2.local>
2019-10-09 08:21:43 -0400

    initial commit

--- Pursuit-Core-iOS-Sets-Lab.playground/Contents.swift
+++ Pursuit-Core-iOS-Sets-Lab.playground/Contents.swift
@@ -3,59 +3,53 @@ import UIKit
 // Uncomment the "assert" lines for each problem when you have a solution, and run the playground to test.  When you have uncommented out the assert lines, no error messages or print statements means that your code is working correctly.
 
 // Question One
-
-// Create a new array numbersWithNoDuplicates that has all of the elements from numbers without any duplicates.  It should be in the same order as the original.
+// Create a new array numbersWithNoDuplicates that has all of the elements from numbers without any duplicates. It should be in the same order as the original.
 
 let numbers = [1,1,2,4,4,4,6,6,7,8]
-
 var numbersWithNoDuplicates = [Int]()
 
-// Your code here
-
-//assert(numbersWithNoDuplicates == [1,2,4,6,7,8], "Was expecting [1,2,4,6,7,8], but got \(numbersWithNoDuplicates)")
+numbersWithNoDuplicates = Set(numbers).sorted()
+assert(numbersWithNoDuplicates == [1,2,4,6,7,8], "Was expecting [1,2,4,6,7,8], but got \(numbersWithNoDuplicates)")
 
 // Questions Two
-
-// Create a new array scoresThatAppearOnce that has all the elements from scores that appear exactly once.  It should be in the same order as the original.
+// Create a new array scoresThatAppearOnce that has all the elements from scores that appear exactly once. It should be in the same order as the original.
 
 let scores = [1, 77, 83, 32, 77, 77, 83, 32, 99]
-
 var scoresThatAppearOnce = [Int]()
 
-// Your code here
-
-//assert(scoresThatAppearOnce == [1, 99], "Was expecting [1, 99], but got \(scoresThatAppearOnce)")
+var scoreCounts = [Int:Int]()
+for score in scores {
+    scoreCounts[score] = (scoreCounts[score] ?? 0) + 1
+}
+for (key,value) in scoreCounts {
+if value == 1 {
+        scoresThatAppearOnce.append(key)
+    }
+}
+scoresThatAppearOnce = scoresThatAppearOnce.sorted()
+assert(scoresThatAppearOnce == [1, 99], "Was expecting [1, 99], but got \(scoresThatAppearOnce)")
 
 // Question Three
-
-// a.
-
-// Given arrOne and arrTwo, create a variable arrThree which is equal to the UNION of arrOne and arrTwo.  It should not contain any duplicate elements.  Sort arrThree from smallest to greatest.
+// a. Given arrOne and arrTwo, create a variable arrThree which is equal to the UNION of arrOne and arrTwo. It should not contain any duplicate elements.  Sort arrThree from smallest to greatest.
 
 let arrOne = [1,2,3,4,5]
 let arrTwo = [3,4,5,6,7]
 
 var arrThree: [Int] = []
+arrThree = Array(Set(arrOne).union(Set((arrTwo)))).sorted()
+assert(arrThree == [1,2,3,4,5,6,7], "Was expecting [1,2,3,4,5,6,7], but got \(arrThree)")
 
-// Your code here
-
-//assert(arrThree == [1,2,3,4,5,6,7], "Was expecting [1,2,3,4,5,6,7], but got \(arrThree)")
-
-// b.
-
-// Given arrFour and arrFive, create a variable arrSix with is equal to the INTERSECTION of arrFour and arrFive.  If should not contain any duplicate elements.  Sort arrSix from smallest to greatest.
+// b. Given arrFour and arrFive, create a variable arrSix with is equal to the INTERSECTION of arrFour and arrFive.  If should not contain any duplicate elements.  Sort arrSix from smallest to greatest.
 
 let arrFour = [1,2,3,4,5]
 let arrFive = [3,4,5,6,7]
 
 var arrSix: [Int] = []
 
-// Your code here
-
-//assert(arrSix == [3,4,5], "Was expecting [3,4,5], but got \(arrSix)")
+arrSix = Array(Set(arrFour).intersection(Set(arrFive))).sorted()
+assert(arrSix == [3,4,5], "Was expecting [3,4,5], but got \(arrSix)")
 
 // Question Four
-
 // Given the 4 arrays of Ints below, create a new array, sorted in ascending order, that contains all the values without duplicates.
 
 let numsOne = [2, 4, 5, 6, 8, 10, 12]
@@ -65,15 +59,11 @@ let numsFour = [1, 3, 4, 5, 6, 7, 9]
 
 var allNumsWithNoDuplicates: [Int] = []
 
-// Your code here
-
-//assert(allNumsWithNoDuplicates == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "Was expecting [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], but got \([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])")
-
+allNumsWithNoDuplicates = Set(numsOne + numsTwo + numsThree + numsFour).sorted()
+assert(allNumsWithNoDuplicates == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "Was expecting [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], but got \([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])")
 
 // Question Five
-
 // Determine if a String is a pangram. A pangram is a string that contains every letter of the alphabet at least once.
-
 // e.g "The quick brown fox jumps over the lazy dog" is a pangram e.g "The quick brown fox jumped over the lazy dog" is NOT a pangram
 
 let strOne = "The quick brown fox jumps over the lazy dog"
@@ -84,11 +74,26 @@ var strOneIsPangram: Bool = false
 var strTwoIsPangram: Bool = false
 var strThreeIsPangram: Bool = false
 
-// Your code here
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var strOneTrimmed = String(Set(strOne.replacingOccurrences(of: " ", with: "").lowercased() ) )
+var orderedStrOne = String(strOneTrimmed.sorted())
 
-//assert(strOneIsPangram == true, "Was expecting true, but got \(strOneIsPangram)")
-//assert(strTwoIsPangram == false, "Was expecting false, but got \(strTwoIsPangram)")
-//assert(strThreeIsPangram == true, "Was expecting false, but got \(strThreeIsPangram)")
+if orderedStrOne == alphabet {
+strOneIsPangram = true
+}
+var strTwoTrimmed = String(Set(strTwo.replacingOccurrences(of: " ", with: "").lowercased() ) )
+var orderedStrTwo = String(strTwoTrimmed.sorted())
+if orderedStrTwo == alphabet {
+strTwoIsPangram = true
+}
+var strThreeTrimmed = String(Set(strThree.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "").lowercased() ) )
+var orderedStrThree = String(strThreeTrimmed.sorted())
+if orderedStrThree == alphabet {
+strThreeIsPangram = true
+}
+assert(strOneIsPangram == true, "Was expecting true, but got \(strOneIsPangram)")
+assert(strTwoIsPangram == false, "Was expecting false, but got \(strTwoIsPangram)")
+assert(strThreeIsPangram == true, "Was expecting false, but got \(strThreeIsPangram)")
 
 
 
